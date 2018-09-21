2a7742ce3768527d5a2659f1db9a8e56bca096a9
HaileyLi <469385231@qq.com>
2018-09-21 14:25:57 -0400

    contact

--- js/background.js
+++ js/background.js
@@ -41,3 +41,58 @@ $(document).scroll(function() {
     bottom: scroll_p / 5
   });
 });
+
+var FollowX = 0,
+  FollowY = 0,
+  x = 0,
+  y = 0,
+  y2 = 0,
+  y3 = 0,
+  x2 = 0,
+  x3 = 0,
+  x4 = 0;
+friction = 1 / 50;
+function moveBG() {
+  x += (FollowX - x) * friction;
+  y += (FollowY - y) * friction;
+
+  x2 += (FollowX - 2 * x2) * friction;
+  x3 += (FollowX - 1.5 * x3) * friction;
+
+  y2 += (FollowY - 2 * y) * friction;
+  y3 += (FollowY - 1.5 * y) * friction;
+
+  translate = "translate(" + x + "px, " + y + "px)";
+  translate2 = "translate(" + x2 + "px, " + y2 + "px)";
+  translate3 = "translate(" + x3 + "px, " + y3 + "px)";
+
+  $(".star-bg").css({
+    "-webkit-transform": translate,
+    "-moz-transform": translate,
+    transform: translate
+  });
+  $(".as-bg").css({
+    "-webkit-transform": translate2,
+    "-moz-transform": translate2,
+    transform: translate2
+  });
+  $(".plane-bg").css({
+    "-webkit-transform": translate3,
+    "-moz-transform": translate3,
+    transform: translate3
+  });
+
+  window.requestAnimationFrame(moveBG);
+}
+
+$(window).on("mousemove click", function(e) {
+  var MouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  var MouseY = Math.max(
+    -100,
+    Math.min(100, $(window).height() / 2 - e.clientY)
+  );
+  FollowX = (20 * MouseX) / 100;
+  FollowY = (10 * MouseY) / 100;
+});
+
+moveBG();
