a89e6dc448ab82fae62d8f215172b89c06fd7afa
HaileyLi <469385231@qq.com>
2018-10-26 14:14:37 -0400

    home

--- js/scroll.js
+++ js/scroll.js
@@ -13,10 +13,3 @@ $(document).ready(function() {
     }
   });
 });
-
-$(".scroll-indicator").click(function() {
-  var href = $(this).attr("href");
-  var pos = $(href).offset().top;
-  $("html,body").animate({ scrollTop: pos }, 1000);
-  return false;
-});
