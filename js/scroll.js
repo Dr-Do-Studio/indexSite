d0c18ce2983095c05a8f02668628857e03a4eff8
HaileyLi <469385231@qq.com>
2018-10-26 12:24:03 -0400

    header bg fix, hamburger menu fix

--- js/scroll.js
+++ js/scroll.js
@@ -2,14 +2,22 @@ $(document).ready(function() {
   $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     if (scroll > 200) {
-      $(".d-header-background").css("background-color", "rgba(0, 0, 0, 0)");
-      $(".d-header-background").css("border-bottom", "none");
       $(".d-header").css("background", "black");
     } else {
-      $(".d-header-background").css("background", "black");
-      $(".d-header-background").css("opacity", scroll / 200);
-      $(".d-header").css("background-color", "rgba(0, 0, 0, 0)");
-      $(".d-header").css("border-bottom", "none");
+      opacity = scroll / 200;
+
+      backgroundopacity = "rgba(0, 0, 0, " + opacity + ")";
+      console.log(backgroundopacity);
+      $(".d-header").css({
+        "background-color": backgroundopacity
+      });
     }
   });
+});
+
+$(".scroll-indicator").click(function() {
+  var href = $(this).attr("href");
+  var pos = $(href).offset().top;
+  $("html,body").animate({ scrollTop: pos }, 1000);
+  return false;
 });
